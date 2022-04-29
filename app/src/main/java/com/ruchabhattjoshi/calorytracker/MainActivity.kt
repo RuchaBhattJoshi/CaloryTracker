31e0282cc2cfa2d1ae025ae6d98c973a675c2ff7
ruchajoshi <joshirucha0807@gmail.com>
2022-04-29 16:28:15 +0100

    Add spacing dimensions and update mainactivity

--- app/src/main/java/com/ruchabhattjoshi/calorytracker/MainActivity.kt
+++ app/src/main/java/com/ruchabhattjoshi/calorytracker/MainActivity.kt
@@ -9,14 +9,15 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.ruchabhattjoshi.calorytracker.ui.theme.CaloryTrackerTheme
+import com.ruchabhattjoshi.onboarding_presentation.welcome.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CaloryTrackerTheme {
-
-                }
+                WelcomeScreen()
             }
         }
     }
+}
