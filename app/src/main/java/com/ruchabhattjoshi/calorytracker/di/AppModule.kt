5f573fbd0d2d4cc87935ef02e064bd5bd3db9392
ruchajoshi <joshirucha0807@gmail.com>
2022-05-21 20:46:38 +0100

    Add DI

---
+++ app/src/main/java/com/ruchabhattjoshi/calorytracker/di/AppModule.kt
@@ -0,0 +1,36 @@
+package com.ruchabhattjoshi.calorytracker.di
+
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import com.ruchabhattjoshi.core.domain.DefaultPreferences
+import com.ruchabhattjoshi.core.domain.preferences.Preferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+/**
+ * Created by ruchajoshi on 21/05/2022
+ */
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(
+        app: Application
+    ): SharedPreferences {
+        return app.getSharedPreferences("shared_pref", MODE_PRIVATE)
+    }
+
+    @Provides
+    @Singleton
+    fun providePreferences(sharedPreferences: SharedPreferences): Preferences {
+        return DefaultPreferences(sharedPreferences)
+    }
+
+}
