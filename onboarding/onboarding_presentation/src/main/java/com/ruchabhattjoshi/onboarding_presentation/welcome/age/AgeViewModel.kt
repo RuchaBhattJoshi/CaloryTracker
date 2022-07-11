bc243e920f5801ec68f05d168e79c8d3a153a5df
ruchajoshi <joshirucha0807@gmail.com>
2022-07-11 20:14:33 +0100

    Add age screen and viewmodel

---
+++ onboarding/onboarding_presentation/src/main/java/com/ruchabhattjoshi/onboarding_presentation/welcome/age/AgeViewModel.kt
@@ -0,0 +1,57 @@
+package com.ruchabhattjoshi.onboarding_presentation.welcome.age
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ruchabhattjoshi.calorytracker.navigation.Route
+import com.ruchabhattjoshi.core.domain.preferences.Preferences
+import com.ruchabhattjoshi.core.domain.use_case.FilterOutDigits
+import com.ruchabhattjoshi.core.navigation.UiText
+import com.ruchabhattjoshi.core.utils.UiEvent
+import com.ruchabhattjoshi.onboarding_presentation.R
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+/**
+ * Created by ruchajoshi on 11/07/2022
+ */
+
+@HiltViewModel
+class AgeViewModel @Inject constructor(
+    private val preferences: Preferences,
+    private val filterOutDigits: FilterOutDigits
+) : ViewModel() {
+
+    var age by mutableStateOf("20")
+        private set
+
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onAgeEnter(age: String) {
+        if (age.length <= 3) {
+            this.age = filterOutDigits(age)
+        }
+    }
+
+    fun onNextClick() {
+        viewModelScope.launch {
+            val ageNumber = age.toIntOrNull() ?: kotlin.run {
+                _uiEvent.send(
+                    UiEvent.ShowSnackbar(
+                        UiText.StringResource(R.string.error_age_cant_be_empty)
+                    )  //hardcoded string need context use UiText
+                )
+
+                return@launch
+            }
+            preferences.saveAge(ageNumber)
+            _uiEvent.send(UiEvent.Navigate(Route.HEIGHT))
+        }
+    }
+}
