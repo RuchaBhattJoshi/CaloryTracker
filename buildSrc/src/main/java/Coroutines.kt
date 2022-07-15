0e76878f7d1124d26f5acf220c609dc650f8717d
ruchajoshi <joshirucha0807@gmail.com>
2022-07-15 23:56:18 +0100

    add dependencies

---
+++ buildSrc/src/main/java/Coroutines.kt
@@ -0,0 +1,7 @@
+/**
+ * Created by ruchajoshi on 15/07/2022
+ */
+object Coroutines {
+    const val version = "1.6.0"
+    const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+}
