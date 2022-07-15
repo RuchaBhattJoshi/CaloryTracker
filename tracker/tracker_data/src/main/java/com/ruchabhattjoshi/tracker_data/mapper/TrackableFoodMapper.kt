ad0ee484e8e21f84bbd224ffcc18cd8578763b57
ruchajoshi <joshirucha0807@gmail.com>
2022-07-15 23:59:06 +0100

    add mappers and implement mappers

---
+++ tracker/tracker_data/src/main/java/com/ruchabhattjoshi/tracker_data/mapper/TrackableFoodMapper.kt
@@ -0,0 +1,23 @@
+package com.ruchabhattjoshi.tracker_data.mapper
+
+import com.ruchabhattjoshi.tracker_data.remote.dto.Product
+import com.ruchabhattjoshi.tracker_domain.repository.model.TrackableFood
+import kotlin.math.roundToInt
+
+/**
+ * Created by ruchajoshi on 15/07/2022
+ */
+fun Product.toTrackableFood(): TrackableFood? {
+    val carbsPer100g = nutriments.carbohydrates100g.roundToInt()
+    val proteinPer100g = nutriments.proteins100g.roundToInt()
+    val fatPer100g = nutriments.proteins100g.roundToInt()
+    val caloriesPer100g = nutriments.energyKcal100g.roundToInt()
+    return TrackableFood(
+        name = productName ?: return null,
+        carbsPer100g = carbsPer100g,
+        proteinPer100g = proteinPer100g,
+        fatPer100g = fatPer100g,
+        caloriesPer100g = caloriesPer100g,
+        imageUrl = imageFrontThumbUrl
+    )
+}
