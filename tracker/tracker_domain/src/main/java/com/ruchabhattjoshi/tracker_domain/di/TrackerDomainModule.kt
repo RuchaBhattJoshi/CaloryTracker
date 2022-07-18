ec637b779e361542a6bf6720cffa57e5c901c772
ruchajoshi <joshirucha0807@gmail.com>
2022-07-18 18:36:48 +0100

    add di for trackerDomainModule

---
+++ tracker/tracker_domain/src/main/java/com/ruchabhattjoshi/tracker_domain/di/TrackerDomainModule.kt
@@ -0,0 +1,34 @@
+package com.ruchabhattjoshi.tracker_domain.di
+
+import com.ruchabhattjoshi.core.domain.preferences.Preferences
+import com.ruchabhattjoshi.tracker_domain.repository.TrackerRepository
+import com.ruchabhattjoshi.tracker_domain.use_case.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+/**
+ * Created by ruchajoshi on 18/07/2022
+ */
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object TrackerDomainModule {
+
+    @ViewModelScoped
+    @Provides
+    fun provideTrackerUseCases(
+        repository: TrackerRepository,
+        preferences: Preferences
+    ): TrackerUseCases {
+        return TrackerUseCases(
+            trackFood = TrackFood(repository),
+            searchFood = SearchFood(repository),
+            getFoodsForDate = GetFoodsForDate(repository),
+            deleteTrackedFood = DeleteTrackedFood(repository),
+            calculateMealNutrients = CalculateMealNutrients(preferences)
+        )
+    }
+}
