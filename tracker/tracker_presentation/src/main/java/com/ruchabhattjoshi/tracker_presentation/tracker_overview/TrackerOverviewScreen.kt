d3d91237ae4103a8e8a3438f94b2bbe53bd0bba5
ruchajoshi <joshirucha0807@gmail.com>
2022-07-19 22:33:51 +0100

    Add NutrientsScreen with components

---
+++ tracker/tracker_presentation/src/main/java/com/ruchabhattjoshi/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -0,0 +1,36 @@
+package com.ruchabhattjoshi.tracker_presentation.tracker_overview
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.ruchabhattjoshi.core.utils.UiEvent
+import com.ruchabhattjoshi.core_ui.LocalSpacing
+import com.ruchabhattjoshi.tracker_presentation.components.NutrientsHeader
+
+
+/**
+ * Created by ruchajoshi on 19/07/2022
+ */
+
+@Composable
+fun TrackerOverviewScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit,
+    viewModel: TrackerOverviewViewModel = hiltViewModel()
+) {
+    val spacing = LocalSpacing.current
+    val state = viewModel.state
+    val context = LocalContext.current
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = spacing.spaceMedium)
+    ) {
+        item {
+            NutrientsHeader(state = state)
+        }
+    }
+}
